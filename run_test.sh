061b4f02afda68aa715011ae6061a06890822fc5
magnitov <mikhail.magnitov@phystech.edu>
2021-01-22 13:46:15 +0300

    Switched from 100kb to 500kb matrix for test
    Former-commit-id: da25992c8f9934de9a70abd114dd56836a40f05d
    Former-commit-id: 2526f63683afc249820660c9c864361e43bd90cf

--- run_test.sh
+++ run_test.sh
@@ -1,21 +1,21 @@
 # Average compartments calculation for sample data
 # Cis
-python ./src/get_pentad_cis.py ./test/Control.100kb.cool ./test/PC1_values.txt::Control --out_pref ./out/Control_cis
-python ./src/get_pentad_cis.py ./test/Hex5.100kb.cool ./test/PC1_values.txt::Hex5 --out_pref ./out/Hex5_cis
+python ./src/get_pentad_cis.py ./test_input/Control.500kb.cool ./test_input/PC1_values_500kb.txt::Control --out_pref ./test_output/Control_cis
+python ./src/get_pentad_cis.py ./test_input/Hex5.500kb.cool ./test_input/PC1_values_500kb.txt::Hex5 --out_pref ./test_output/Hex5_cis
 # Trans
-python ./src/get_pentad_trans.py ./test/Control.100kb.cool ./test/PC1_values.txt::Control --out_pref ./out/Control_trans
-python ./src/get_pentad_trans.py ./test/Hex5.100kb.cool ./test/PC1_values.txt::Hex5 --out_pref ./out/Hex5_trans
+python ./src/get_pentad_trans.py ./test_input/Control.500kb.cool ./test_input/PC1_values_500kb.txt::Control --out_pref ./test_output/Control_trans
+python ./src/get_pentad_trans.py ./test_input/Hex5.500kb.cool ./test_input/PC1_values_500kb.txt::Hex5 --out_pref ./test_output/Hex5_trans
 # By distance
-python ./src/get_pentad_distance.py ./test/Control.100kb.cool ./test/PC1_values.txt::Control --out_pref ./out/Control_distance --distance 10 50 100
-python ./src/get_pentad_distance.py ./test/Hex5.100kb.cool ./test/PC1_values.txt::Hex5 --out_pref ./out/Hex5_distance --distance 10 50 100
+python ./src/get_pentad_distance.py ./test_input/Control.500kb.cool ./test_input/PC1_values_500kb.txt::Control --out_pref ./test_output/Control_distance --distance 10 25
+python ./src/get_pentad_distance.py ./test_input/Hex5.500kb.cool ./test_input/PC1_values_500kb.txt::Hex5 --out_pref ./test_output/Hex5_distance --distance 10 25
 
 # Visualization of calculated average compartments for sample data
 # Cis
-python ./src/plot_pentad.py ./out/Control_cis.json --title Control --out_pref ./out/Control_cis_plot
-python ./src/plot_pentad.py ./out/Hex5_cis.json --title Hex5 --out_pref ./out/Hex5_cis_plot
+python ./src/plot_pentad.py ./test_output/Control_cis.json --title Control --vmin 0.5 --vmax 2 --out_pref ./test_output/Control_cis_plot
+python ./src/plot_pentad.py ./test_output/Hex5_cis.json --title Hex5 --vmin 0.5 --vmax 2 --out_pref ./test_output/Hex5_cis_plot
 # Trans
-python ./src/plot_pentad.py ./out/Control_trans.json --title Control --out_pref ./out/Control_trans_plot
-python ./src/plot_pentad.py ./out/Hex5_trans.json --title Hex5 --out_pref ./out/Hex5_trans_plot
+python ./src/plot_pentad.py ./test_output/Control_trans.json --title Control --vmin 0.4 --vmax 2.5 --out_pref ./test_output/Control_trans_plot
+python ./src/plot_pentad.py ./test_output/Hex5_trans.json --title Hex5 --vmin 0.4 --vmax 2.5 --out_pref ./test_output/Hex5_trans_plot
 # By distance
-python ./src/plot_pentad.py ./out/Control_distance.json --title Control --out_pref ./out/Control_distance_plot
-python ./src/plot_pentad.py ./out/Hex5_distance.json --title Hex5 --out_pref ./out/Hex5_distance_plot
+python ./src/plot_pentad.py ./test_output/Control_distance.json --title Control --vmin 0.5 --vmax 2 --out_pref ./test_output/Control_distance_plot
+python ./src/plot_pentad.py ./test_output/Hex5_distance.json --title Hex5 --vmin 0.5 --vmax 2 --out_pref ./test_output/Hex5_distance_plot
