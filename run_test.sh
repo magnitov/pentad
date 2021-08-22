ea06e51bfad20fd536f81f254cf5a34504b8f2a0
magnitov <mikhail.magnitov@phystech.edu>
2021-08-22 17:15:14 +0300

    Update of the test data

--- run_test.sh
+++ run_test.sh
@@ -1,21 +1,42 @@
+# Download sample data
+wget -O Ulianov_et_al_Control.mcool https://ftp.ncbi.nlm.nih.gov/geo/series/GSE138nnn/GSE138543/suppl/GSE138543_Control.mcool
+wget -O Ulianov_et_al_Hex5.mcool https://ftp.ncbi.nlm.nih.gov/geo/series/GSE138nnn/GSE138543/suppl/GSE138543_Hex5.mcool
+wget -O Ulianov_et_al_Recovery.mcool https://ftp.ncbi.nlm.nih.gov/geo/series/GSE138nnn/GSE138543/suppl/GSE138543_Recovery.mcool
+wget -O compartment_signal.txt.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE138nnn/GSE138543/suppl/GSE138543_compartment_signal.100kb.txt.gz
+gunzip compartment_signal.txt.gz
+
 # Average compartments calculation for sample data
-# Cis
-python ./src/get_pentad_cis.py ./test_input/Control.500kb.cool ./test_input/PC1_values_500kb.txt::Control --out_pref ./test_output/Control_cis
-python ./src/get_pentad_cis.py ./test_input/Hex5.500kb.cool ./test_input/PC1_values_500kb.txt::Hex5 --out_pref ./test_output/Hex5_cis
-# Trans
-python ./src/get_pentad_trans.py ./test_input/Control.500kb.cool ./test_input/PC1_values_500kb.txt::Control --out_pref ./test_output/Control_trans
-python ./src/get_pentad_trans.py ./test_input/Hex5.500kb.cool ./test_input/PC1_values_500kb.txt::Hex5 --out_pref ./test_output/Hex5_trans
-# By distance
-python ./src/get_pentad_distance.py ./test_input/Control.500kb.cool ./test_input/PC1_values_500kb.txt::Control --out_pref ./test_output/Control_distance --distance 10 25
-python ./src/get_pentad_distance.py ./test_input/Hex5.500kb.cool ./test_input/PC1_values_500kb.txt::Hex5 --out_pref ./test_output/Hex5_distance --distance 10 25
+for DATASET in Control Hex5 Recovery
+do
+	# Cis
+	python ./src/get_pentad_cis.py Ulianov_et_al_${DATASET}.mcool::resolutions/100000 compartment_signal.txt::${DATASET} --out_pref ./test/${DATASET}_cis
+	# Trans
+	python ./src/get_pentad_trans.py Ulianov_et_al_${DATASET}.mcool::resolutions/100000 compartment_signal.txt::${DATASET} --max_zeros 0.5 --out_pref ./test/${DATASET}_trans
+	# By distance
+	python ./src/get_pentad_distance.py Ulianov_et_al_${DATASET}.mcool::resolutions/100000 compartment_signal.txt::${DATASET} --out_pref ./test/${DATASET}_distance --distance 10 25
+done
 
 # Visualization of calculated average compartments for sample data
-# Cis
-python ./src/plot_pentad.py ./test_output/Control_cis.json --title Control --vmin 0.5 --vmax 2 --out_pref ./test_output/Control_cis_plot
-python ./src/plot_pentad.py ./test_output/Hex5_cis.json --title Hex5 --vmin 0.5 --vmax 2 --out_pref ./test_output/Hex5_cis_plot
-# Trans
-python ./src/plot_pentad.py ./test_output/Control_trans.json --title Control --vmin 0.4 --vmax 2.5 --out_pref ./test_output/Control_trans_plot
-python ./src/plot_pentad.py ./test_output/Hex5_trans.json --title Hex5 --vmin 0.4 --vmax 2.5 --out_pref ./test_output/Hex5_trans_plot
-# By distance
-python ./src/plot_pentad.py ./test_output/Control_distance.json --title Control --vmin 0.5 --vmax 2 --out_pref ./test_output/Control_distance_plot
-python ./src/plot_pentad.py ./test_output/Hex5_distance.json --title Hex5 --vmin 0.5 --vmax 2 --out_pref ./test_output/Hex5_distance_plot
+for DATASET in Control Hex5 Recovery
+do
+	# Cis
+	python ./src/plot_pentad.py ./test/${DATASET}_cis.json --title ${DATASET} --out_pref ./test/${DATASET}_cis_plot
+	# Trans
+	python ./src/plot_pentad.py ./test/${DATASET}_trans.json --title ${DATASET} --vmin 0.05 --vmax 20 --out_pref ./test/${DATASET}_trans_plot
+	# By distance
+	python ./src/plot_pentad.py ./test/${DATASET}_distance.json --title ${DATASET} --out_pref ./test/${DATASET}_distance_plot
+done
+
+# Average compartment strength quantification
+for DATASET in Control Hex5 Recovery
+do
+	# Cis
+	python ./src/quant_strength_cis.py Ulianov_et_al_${DATASET}.mcool::resolutions/100000 compartment_signal.txt::${DATASET} --out_pref ./test/${DATASET}_cis
+	# Trans
+	python ./src/quant_strength_trans.py Ulianov_et_al_${DATASET}.mcool::resolutions/100000 compartment_signal.txt::${DATASET} --max_zeros 0.5 --out_pref ./test/${DATASET}_trans
+	# By distance
+	python ./src/quant_strength_distance.py Ulianov_et_al_${DATASET}.mcool::resolutions/100000 compartment_signal.txt::${DATASET} --out_pref ./test/${DATASET}_distance --distance 10 25
+done
+
+# Remove sample data
+rm *.mcool compartment_signal.txt
